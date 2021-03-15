96ca634f2ae6dba886fc77b7a6b8c65a38a70391
Jeffery Davis <jef.davis@live.com>
2021-03-15 15:42:35 -0500

    deploying

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/myFirstDatabase", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
