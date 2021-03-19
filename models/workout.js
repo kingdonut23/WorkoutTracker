6761bffa9dd34090d1773ad3da861d31fae0644f
Jeffery Davis <jef.davis@live.com>
2021-03-18 21:26:23 -0500

    Fixed again fool

--- models/workout.js
+++ models/workout.js
@@ -2,45 +2,49 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema ({
-    exercise:{
-        type: String,
-        trim: true,
-        required: "Choose your exercise"
-    },
+const workoutSchema = new Schema({
 
-    name:{
-        type: String,
-        trim: true,
-        required: "What exercise?"
-    },
-
-    weight:{
-        type:Number
-    },
-
-    reps:{
-        type: Number
-    },
-    
-    sets:{
-        type:Number
-
-    },
-    duration:{
-        type: Number,
-        required:"How long did you do this exercise?"
-    },
-
-    distance:{
-        type: Number
-    },
-///can't decide if I want the day to exist in the array or out
-    day:{
-        type: Date, 
+    ///can't decide if I want the day to exist in the array or out
+    day: {
+        type: Date,
         default: Date.now
-    }
+    },
 
+    exercises: [{
+        type: {   
+            type: String,
+            trim: true,
+            required: "Choose your exercise"
+        },
+
+        name: {
+            type: String,
+            trim: true,
+            required: "What exercise?"
+        },
+
+        duration: {
+            type: Number,
+            required: "How long did you do this exercise?"
+        },
+
+        weight: {
+            type: Number
+        },
+
+        reps: {
+            type: Number
+        },
+
+        sets: {
+            type: Number
+
+        },
+
+        distance: {
+            type: Number
+        }
+     }]
 });
 
 const workout = mongoose.model("workout", workoutSchema);
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,30 @@
 const models = require("../models/");
 const router = require("express").Router();
 
-
-module.exports = function (router) {
-    /// new workout
+   /// new workout
     router.post("/api/workouts", function (req, res) {
         models.Workout.create(req.body)
             .then(function (workout) {
+                console.log(workout);
                 res.json(workout);
-                console.log(workout)
             }).catch(function (err) {
                 res.status(400).json(err);
             });
     });
+
+    router.put("/api/workouts/:id", function (req, res) {
+        console.log(req.body)
+        models.Workout.findByIdAndUpdate(req.params.id, {
+            $push: { exercises: req.body }
+        })
+            .then(function (workout) {
+                console.log(workout);
+                res.json(workout);
+            }).catch(function (err) {
+                res.status(400).json(err);
+            });
+    });
+
     /// stop giving me trouble and work!
     // supposed to pull workouts
     router.get("/api/workouts", function (req, res) {
@@ -36,4 +48,4 @@ module.exports = function (router) {
 
     })
 
-}
+    module.exports = router;
